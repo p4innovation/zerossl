38bc7ff0715d6e93b1bcb676b377b7a343588358
Mike Howson <mike.howson@p4innovation.com>
2024-04-15 09:57:52 +0100

    Update config.rb

--- lib/zerossl/config.rb
+++ lib/zerossl/config.rb
@@ -5,9 +5,9 @@ module ZeroSSL
   class Setup
     extend Dry::Configurable
 
-    setting :api_uri,    'https://api.zerossl.com'
-    setting :access_key, nil
-    setting :logger,     Logger.new(STDOUT)
-    setting :user_agent, "ZeroSSL Ruby Client / #{ZeroSSL::VERSION}"
+    setting :api_uri, default: 'https://api.zerossl.com'
+    setting :access_key, default: nil
+    setting :logger, default: Logger.new(STDOUT)
+    setting :user_agent, default: "ZeroSSL Ruby Client / #{ZeroSSL::VERSION}"
   end
 end
